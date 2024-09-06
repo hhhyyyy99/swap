d1c5f4fb6a2c36ded52387cbc8896eb1f3f68cf3
Eamon <1025942836@qq.com>
2024-09-06 10:22:20 +0800

    fix: update defaultWallet

--- src/config/walletConnect.ts
+++ src/config/walletConnect.ts
@@ -1,5 +1,5 @@
 import type { WalletConnectParameters } from 'wagmi/connectors';
 
 export const defaultWalletConnectConfig: WalletConnectParameters = {
-  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
 };
